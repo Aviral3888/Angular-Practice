b054686c98181de99e55a46019280899a5b5efa8
Aviral Goel <avigoel.3888@gmail.com>
2025-09-13 20:53:25 +0530

    Ng: HttpClient - api integrated

--- YT/crash-course/first-ng-app/src/app/services/todos.service.ts
+++ YT/crash-course/first-ng-app/src/app/services/todos.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-
-  public todoItems: Array<Todo> = [
-    { title: "Groceries", id: 1, userId: 1, completed: false },
-    { title: "Car wash", id: 2, userId: 1, completed: false },
-    { title: "Library", id: 3, userId: 1, completed: false },
-    { title: "Barber", id: 4, userId: 1, completed: false }
-  ]
+  public http = inject(HttpClient);
 
   constructor() {
     console.log("todos.service loaded");
   }
+
+  fetchTodosFromApi() {
+    let url = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get<Array<Todo>>(url);
+  }
+
 }
--- YT/crash-course/first-ng-app/src/app/todos/todos.component.ts
+++ YT/crash-course/first-ng-app/src/app/todos/todos.component.ts
@@ -2,25 +2,38 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { TodoItemsComponent } from '../components/todo-items/todo-items.component';
 import { Todo } from '../model/todo.type';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemsComponent, CommonModule],
+  imports: [TodoItemsComponent, CommonModule, NgIf],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit {
   public todoService = inject(TodosService);
   public todoItems = signal<Array<Todo>>([]);
+  public todoLoader: boolean = true;
 
   constructor() {
     console.log("todos.component loaded");
   }
 
   ngOnInit(): void {
-    console.log(this.todoService.todoItems);
-    this.todoItems.set(this.todoService.todoItems);
+
+    this.todoService.fetchTodosFromApi()
+      .pipe(
+        catchError((error) => {
+          console.log("Error:", error);
+          throw error;
+        })
+      ).subscribe((todos) => {
+        console.log("Todos:", todos);
+        this.todoItems.set(todos);
+        this.todoLoader = false;
+      });
+
   }
 
 }
